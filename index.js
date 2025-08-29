c67cbcdb23575c5ed4a4496a46b9027da9c55ee1
akib-naimul <akibnaimul2000@gmail.com>
2025-08-29 11:33:53 +0600

    added hearts and call func

---
+++ index.js
@@ -0,0 +1,35 @@
+// Cards HeartIcon click ==> COunt in NavBar hearts icon
+
+const likeCount = document.getElementById("like-count"); //navbar like p tag
+const heartIcons = document.querySelectorAll(".card-logo2");   // catch all the hearticons of all cards
+
+for (const heart of heartIcons){
+    heart.addEventListener("click",function(){
+        likeCount.innerText = parseInt(likeCount.innerText) + 1;
+    });
+}
+
+
+//  CALL BUTTON Features:
+
+const coinCount = document.getElementById("coins");  //get the navbar coins tag
+const callHistoryContainer = document.getElementById("call-history-cards");   //mother call history coinatiner
+
+const callBtns = document.querySelectorAll(".call-btn");    // get call buttons from cards
+
+for(const btn of callBtns){
+    btn.addEventListener("click",function(){
+        let currentcoin = parseInt(coinCount.innerText);
+
+        if (currentcoin < 20){
+            alert("Not enought coins");
+            return;
+        }
+        else{
+            coinCount.innerText = currentcoin - 20;
+        }
+    })
+}
+
+
+
